9be9b91d6399385aebedf3d07195be698860e982
QAPT12 <tuckquintin@hotmail.com>
2024-05-31 09:11:55 -0230

    A1 Finished

--- Assignments/Assignment1/process_cargo.js
+++ Assignments/Assignment1/process_cargo.js
@@ -9,24 +9,26 @@ $(document).ready(function() {
     $("#processCargoButton").on("click", process_cargo);
 });
 
+var totalCarWeight = 15000;
+var totalCargoWeight = 0;
+
 function clear_cargo_inputs() {
     $("#cargoID, #cargoDescription, #cargoWeight").val('');
 }
 
 function process_cargo() {
+    let boxID = $("#boxCarID").val();
     let id = $("#cargoID").val();
     let description = $("#cargoDescription").val();
     let weight = $("#cargoWeight").val();
     if($("#cargoTable").length == 0){
-        create_cargo_table();
+        create_cargo_table(boxID);
     }
     add_cargo_row(id, description, weight);
     clear_cargo_inputs();
 }
 
 function add_cargo_row(id, description, weight) {
-    console.log("adding row");
-    let table = $("#cargoTable");
 
     const new_row = `
         <tr>
@@ -36,22 +38,38 @@ function add_cargo_row(id, description, weight) {
         </tr>
     `;
 
-    table.find("tbody").append(new_row);
-}
+    totalCarWeight += parseInt(weight);
+    totalCargoWeight += parseInt(weight);
 
-function create_cargo_table() {
+    $("#boxCarWeight").val(totalCarWeight);
+
+    $('#cargoWeightSummaryRow').before(new_row);
+    $('#cargoWeightSummaryRow td').eq(2).text(totalCargoWeight);
+}   
+
+function create_cargo_table(boxID) {
+    const manifest_title = `
+        <h3>Cargo Box Car Manifest for Box Car ${boxID}</h3>
+    `;
+
     const new_table = `
         <table id="cargoTable">
             <thead>
-                <th>Transport ID</th>
-                <th>Description</th>
-                <th>Weight</th>
+                <tr>
+                    <th>Transport ID</th>
+                    <th>Description</th>
+                    <th>Cargo Weight</th>
+                </tr>
             </thead>
             <tbody>
-
+                <tr id="cargoWeightSummaryRow">
+                    <td></td>
+                    <td>Total Cargo Weight</td>
+                    <td>0</td>
+                </tr>
             </tbody>
         </table>
     `;
 
-    $("body").append(new_table);
+    $("body").append(manifest_title, new_table);
 }
