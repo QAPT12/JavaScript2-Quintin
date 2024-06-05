9cce6603b26bc39fed362677f54ca1b38b889c61
quintin.tuck <tuckquintin@hotmail.com>
2024-06-05 11:42:09 -0230

    Assignment 2 Functional

--- Assignments/Assignment2/process_cargo.js
+++ Assignments/Assignment2/process_cargo.js
@@ -8,49 +8,103 @@ $(document).ready(function() {
     // Add events to our buttons
     $("#cargoResetButton").on("click", clear_cargo_inputs);
     $("#processCargoButton").on("click", process_cargo);
+    
     // set value for total car weight and max car weight
     emptyCarWeight = parseInt($("#boxCarEmpty").val());
     totalCarWeight = parseInt($("#boxCarEmpty").val());
     maxCarWeight = parseInt($("#boxCarMaxWeight").val());
-
 });
 
 function clear_cargo_inputs() {
-    $("#cargoID, #cargoDescription, #cargoWeight").val('');
+    $("#cargoForm input").val('');
+}
+
+function clear_spans() {
+    $("span").text("");
 }
 
 function process_cargo() {
     let submissionError = false;
+    clear_spans();
 
     let boxID = $("#boxCarID").val();
     let id = $("#cargoID").val().trim();
     let description = $("#cargoDescription").val().trim();
     let weight = parseInt($("#cargoWeight").val());
+    if($("#statusTable").length == 0){
+        create_status_table();
+    }
     if($("#manifestTable").length == 0){
         create_manifest_table(boxID);
     }
+    
 
-    if(id == ""){
+    if(id == "") {
         submissionError = true;
         $("#cargoID").next().text("Transport ID is required");
     }
-    if(description == ""){
+    if(description == "") {
         submissionError = true;
         $("#cargoDescription").next().text("Cargo Item must have description");
     }
-    if(isNaN(weight)){
+    if(isNaN(weight)) {
         submissionError = true;
         $("#cargoWeight").next().text("Cargo Weight must be numerical");
-    } else if(weight < 1 || weight + emptyCarWeight > maxCarWeight){
+    } else if(weight < 1) {
         submissionError = true;
-        $("#cargoWeight").next().text("Cargo weight must be greater than 1 and cannot push Car above max weight");
+        $("#cargoWeight").next().text("Cargo weight must be greater than 1");
+    } else if(weight + emptyCarWeight > maxCarWeight) {
+        submissionError = true;
+        $("#cargoWeight").next().text("Cargo weight + Empty cannot exceed Max");
     }
 
-    if(!submissionError){
-        add_manifest_row(id, description, weight);
-        clear_cargo_inputs();
+    if(!submissionError) {
+        if(weight + totalCarWeight > maxCarWeight) {
+            add_status_row(id, description, weight, "Warehouse");
+        } else {
+            add_status_row(id, description, weight, boxID);
+            add_manifest_row(id, description, weight);
+            clear_cargo_inputs();
+        }
+        
     }
     
+}
+
+function create_status_table() {
+    const status_title = `
+        <h3>Cargo Status</h3>
+    `;
+
+    const new_table = `
+        <table id="statusTable">
+            <thead>
+                <tr>
+                    <th>Transport ID</th>
+                    <th>Description</th>
+                    <th>Weight</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    `;
+
+    $("body").append(status_title, new_table);
+}
+
+function add_status_row(id, description, weight, status){
+    const new_row = `
+        <tr>
+            <td>${id}</td>
+            <td>${description}</td>
+            <td>${weight}</td>
+            <td>${status}</td>
+        </tr>
+    `;
+
+    $("#statusTable").append(new_row);
 }
 
 function add_manifest_row(id, description, weight) {
@@ -82,13 +136,13 @@ function create_manifest_table(boxID) {
                 <tr>
                     <th>Transport ID</th>
                     <th>Description</th>
-                    <th>Cargo Weight</th>
+                    <th>Weight</th>
                 </tr>
             </thead>
             <tbody>
                 <tr id="cargoWeightSummaryRow">
                     <td></td>
-                    <td>Total Cargo Weight</td>
+                    <td>Total Weight</td>
                     <td>0</td>
                 </tr>
             </tbody>
