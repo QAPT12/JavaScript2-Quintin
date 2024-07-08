752bc8715fcb7db8a0c7fac2fefeeda64428f70c
quintin.tuck <tuckquintin@hotmail.com>
2024-07-08 11:33:12 -0230

    further progress. Handling for boxcar input.

--- Project/Cargo.js
+++ Project/Cargo.js
@@ -1,5 +1,7 @@
 class Cargo {
 
+    // Us this class not object literal. Add in a status, will be needed for later anyways
+
     constructor(transportID, description, weight){
         this.transportID = transportID;
         this.description = description;
--- Project/main.js
+++ Project/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $(".return-button").on("click", return_to_main);
-    add_menu_button_handlers();
+    add_button_handlers();
 });
 
+// Functions for the main menu/ whole scope
 function return_to_main() {
-    $(".menuDiv").not("#divA").hide();
+    $(".menu-div").not("#divA").hide();
     $("#divA").show();
     $("input[type=radio][name=menu]").prop("checked", false);
 }
@@ -12,12 +13,47 @@ function return_to_main() {
 function switch_div(event) {
     let currentElement = event.currentTarget;
     let div = "#" + currentElement.value;
-    $(".menuDiv").not(div).hide();
+    $(".menu-div").not(div).hide();
     $(div).show();
+}
+
+function clear_form(formID){
+    console.log("clearing form " + formID);
+    $(formID + " input").val("");
 }
 
 function add_menu_button_handlers() {
     console.log("adding handlers to menu buttons");
     let all_buttons = $("input[type=radio][name=menu]");
     all_buttons.change(switch_div);
+}
+
+function add_form_reset_handlers(){
+    console.log("adding handlers to form reset buttons");
+    $(".reset-form-button").on("click", function(){
+        let parent_form_id = $(this).parent().parent().attr("id");
+        let form = "#" + parent_form_id;
+        clear_form(form);
+    })
+}
+
+function add_button_handlers(){
+    add_menu_button_handlers();
+    add_form_reset_handlers();
+    
+    $("#boxCarID").on("keyup", boxcar_id_check);
+}
+
+// Functions for creating the Box Cars
+function boxcar_id_check() {
+    let input = $("#boxCarID").val();
+    const regex = new RegExp("^BX\\d{3}$");
+    if(regex.test(input)){
+        console.log("good input");
+        $("#boxCarErrorMsg").text("");
+        
+    } else {
+        console.log("bad input");
+        $("#boxCarErrorMsg").text("ID Must be BX followed by 3 digits");
+    }
 }
